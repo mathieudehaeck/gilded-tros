96711519c6d6659ea2ecd5830d80fdb58b5e4362
Mathieu De haeck <mathieu.de.haeck@gmail.com>
2026-02-11 11:00:26 +0100

    chore: configure Jest to ignore dist folder
    - Add testPathIgnorePatterns to exclude /dist/ from test runs
    - Prevents duplicate test execution and Babel configuration errors
    - Tests now run cleanly with only TypeScript source files

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "ts-jest",
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
   transform: {
     "^.+\\.(ts|tsx)?$": "ts-jest",
     "^.+\\.(js|jsx)$": "babel-jest",
