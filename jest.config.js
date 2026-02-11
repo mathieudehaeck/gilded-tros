d3c9bc52a18d7628b09d61c9933d9ab783a3eb0a
Mathieu De haeck <mathieu.de.haeck@gmail.com>
2026-02-11 16:12:11 +0100

    chore: update dependencies and added test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   preset: "ts-jest",
+  testEnvironment: "node",
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
-  transform: {
-    "^.+\\.(ts|tsx)?$": "ts-jest",
-    "^.+\\.(js|jsx)$": "babel-jest",
+  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.d.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
   },
 };
